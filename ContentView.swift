cf2a59ce23c0ca373160bee8760d7669361cf3ee
A <ajaygeorge@me.com>
2023-03-30 10:40:12 -0500

    Updated for sequential form inputs

--- ContentView.swift
+++ ContentView.swift
@@ -8,77 +8,89 @@ struct ContentView: View {
     @State private var hobbies: String = ""
     @State private var favorites: String = ""
     @State private var personality: String = ""
-    @State private var strengths: String = ""
-    @State private var weaknesses: String = ""
-    @State private var goals: String = ""
-    @State private var fears: String = ""
     
     var body: some View {
         NavigationView {
             Group {
                 VStack(alignment: .leading) {
+                    Text("Let's Talk About Your Loved One")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .padding(.bottom, 20)
+                    
                     Text("What is the name of your loved one?")
                     TextField("Enter name", text: $name)
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                         .padding(.bottom, 10)
                     
-                    Text("How old do you want \(name) to be in this conversation?")
-                    TextField("Enter age", text: $age)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .padding(.bottom, 10)
+                    if !name.isEmpty {
+                        Text("How old do you want \(name) to be in this conversation?")
+                        TextField("Enter age", text: $age)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .padding(.bottom, 10)
+                    }
                     
-                    Text("\(name)'s gender")
-                    TextField("Enter gender", text: $gender)
+                    if !age.isEmpty {
+                        Text("\(name)'s gender")
+                        Picker("Gender", selection: $gender) {
+                            Text("Male").tag("Male")
+                            Text("Female").tag("Female")
+                            Text("Other").tag("Other")
+                        }
+                        .pickerStyle(MenuPickerStyle())
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                         .padding(.bottom, 10)
+                    }
                     
-                    Text("\(name)'s occupation")
-                    TextField("Enter occupation", text: $occupation)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .padding(.bottom, 10)
+                    if !gender.isEmpty {
+                        Text("\(name)'s occupation")
+                        TextField("Enter occupation", text: $occupation)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .padding(.bottom, 10)
+                    }
+                    
+                    if !occupation.isEmpty {
+                        Text("\(name)'s hobbies or interests")
+                        TextField("Enter hobbies or interests", text: $hobbies)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .padding(.bottom, 10)
+                    }
+                    
+                    if !hobbies.isEmpty {
+                        Text("\(name)'s favorite books, movies, or TV shows")
+                        TextField("Enter favorites", text: $favorites)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .padding(.bottom, 10)
+                    }
+                    
+                    if !favorites.isEmpty {
+                        Text("How would you describe \(name)'s personality in a few words?")
+                        TextField("Enter personality", text: $personality)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .padding(.bottom, 10)
+                    }
                 }
                 
-                VStack(alignment: .leading) {
-                    Text("\(name)'s hobbies or interests")
-                    TextField("Enter hobbies or interests", text: $hobbies)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .padding(.bottom, 10)
+                VStack {
+                    Spacer()
                     
-                    Text("\(name)'s favorite books, movies, or TV shows")
-                    TextField("Enter favorites", text: $favorites)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .padding(.bottom, 10)
-                    
-                    Text("How would you describe \(name)'s personality in a few words?")
-                    TextField("Enter personality", text: $personality)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .padding(.bottom, 10)
-                }
-                
-                VStack(alignment: .leading) {
-                    Text("\(name)'s strengths")
-                    TextField("Enter strengths", text: $strengths)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .padding(.bottom, 10)
-                    
-                    Text("\(name)'s weaknesses")
-                    TextField("Enter weaknesses", text: $weaknesses)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .padding(.bottom, 10)
-                    
-                    Text("\(name)'s goals or ambitions")
-                    TextField("Enter goals", text: $goals)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .padding(.bottom, 10)
-                    
-                    Text("\(name)'s fears or challenges")
-                    TextField("Enter fears", text: $fears)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .padding(.bottom, 10)
+                    Button(action: {
+                        // Start talking action
+                    }) {
+                        Text("Start talking")
+                            .foregroundColor(.white)
+                            .font(.headline)
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(Color.blue)
+                            .cornerRadius(10)
+                            .padding()
+                    }
                 }
             }
             .padding()
-            .navigationBarTitle("Character Information")
+            .navigationBarTitle("")
+            .navigationBarHidden(true)
         }
         .navigationViewStyle(StackNavigationViewStyle())
         .preferredColorScheme(.dark)
